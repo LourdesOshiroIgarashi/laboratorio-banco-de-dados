39e60cc90df8ead5a98a77a7c2947360b32c31cf
Lourdes Oshiro Igarashi <83931936+LourdesOshiroIgarashi@users.noreply.github.com>
2023-08-21 16:15:57 -0400

    Update 21-08_incremento_2_id.sql

--- aulas/21-08_incremento_2_id.sql
+++ aulas/21-08_incremento_2_id.sql
@@ -10,8 +10,6 @@ values ('Tiago'), ('Lourdes'), ('Vinicius'), ('Lívia'), ('Gustavo');
 
 DROP TABLE ALUNO;
 
-INSERT INTO aluno (nome) values ('aluno1');
-
 ALTER SEQUENCE alunoid INCREMENT 2;
 
 select * from aluno;
