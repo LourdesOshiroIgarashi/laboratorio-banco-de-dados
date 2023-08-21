58a13367f5b86698b114f18a0b7fcf8ddad439c2
Lourdes Oshiro Igarashi <83931936+LourdesOshiroIgarashi@users.noreply.github.com>
2023-08-21 16:12:13 -0400

    Create 21_08_tabela_aluno.sql

---
+++ aulas/21_08_tabela_aluno.sql
@@ -0,0 +1,17 @@
+-- Criação da tabela aluno com id e nome do aluno
+
+CREATE TABLE aluno(
+	id INTEGER DEFAULT NEXTVAL('alunoid') PRIMARY KEY,
+	nome VARCHAR(200)
+);
+
+CREATE SEQUENCE alunoid;
+
+INSERT INTO aluno (nome)
+values ('Tiago'), ('Lourdes'), ('Vinicius'), ('Lívia'), ('Gustavo');
+
+INSERT INTO aluno (nome) values ('aluno1');
+
+select * from aluno;
+
+DROP TABLE ALUNO;
